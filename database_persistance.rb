03fdcc00b08d00adafed5c316816c85e7b9fa1db
Cody Thomas <cody.t.thomas@gmail.com>
2023-01-16 12:35:36 -0700

    Budget total will now calculate correctly even if relevant expenses are not yet added

--- database_persistance.rb
+++ database_persistance.rb
@@ -172,13 +172,8 @@ class DatabasePersistance
     sql = <<~SQL
       SELECT SUM(max_amount) 
         FROM categories
-        WHERE id IN (
-          SELECT category_id
-            FROM expenses
-            WHERE DATE_PART('month', expense_date) = $1
-        )
     SQL
-    result = query(sql, CURRENT_DATE.month)
+    result = query(sql)
     '%.2f' % result.first['sum']
   end
 
